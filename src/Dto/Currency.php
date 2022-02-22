b7956d9d0ed73bc35af5b3a90a4d1da21728df44
evotodi <evotodi@gmail.com>
2022-02-22 09:41:04 -0500

    Version 0.0.3 Fixed bug in Currency.php

--- src/Dto/Currency.php
+++ src/Dto/Currency.php
@@ -4,13 +4,13 @@ namespace Evotodi\WaveBundle\Dto;
 
 class Currency
 {
-    public string $code;
-    public string $symbol;
-    public string $name;
-    public string $plural;
-    public int $exponent;
+    public ?string $code;
+    public ?string $symbol;
+    public ?string $name;
+    public ?string $plural;
+    public ?int $exponent;
 
-    public function __construct(string $code, string $symbol, string $name, string $plural, int $exponent)
+    public function __construct(?string $code, ?string $symbol, ?string $name, ?string $plural, ?int $exponent)
     {
         $this->code = $code;
         $this->symbol = $symbol;
